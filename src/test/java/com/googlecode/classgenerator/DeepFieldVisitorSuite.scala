fb53d4a12948527de797457967a179440a0f8f4b
kostas.kougios <kostas.kougios@gmail.com>
2012-06-04 11:41:37 +0000

    deep field visitor

--- src/test/java/com/googlecode/classgenerator/DeepFieldVisitorSuite.scala
+++ src/test/java/com/googlecode/classgenerator/DeepFieldVisitorSuite.scala
@@ -16,16 +16,28 @@ class DeepFieldVisitorSuite extends FunSuite with ShouldMatchers {
 	val rm = new ReflectionManager
 	val dfv = new DeepFieldVisitor(rm)
 	test("deep visit") {
-		val result = dfv.visit(Person("kostas", Dog("greta"))) { (o, field, v) =>
+		val result = dfv.visit(Person("kostas", Dog("greta", 5), 20)) { (o, field, v) =>
 			(field.getName, v)
 		}
 		result.toSet should be === Set(
 			("name", "kostas"),
-			("pet", Dog("greta")),
-			("name", "greta")
+			("pet", Dog("greta", 5)),
+			("age", 20),
+			("name", "greta"),
+			("age", 5)
+		)
+	}
+	test("deep visit, nulls") {
+		val result = dfv.visit(Person("kostas", null, 20)) { (o, field, v) =>
+			(field.getName, v)
+		}
+		result.toSet should be === Set(
+			("name", "kostas"),
+			("pet", null),
+			("age", 20)
 		)
 	}
 
-	case class Person(name: String, pet: Dog)
-	case class Dog(name: String)
+	case class Person(name: String, pet: Dog, age: Int)
+	case class Dog(name: String, age: Int)
 }
