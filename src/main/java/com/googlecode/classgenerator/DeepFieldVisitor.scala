2b1615d936e457e5dd061a8b2bb3d3778743ed3b
kostas.kougios <kostas.kougios@gmail.com>
2012-06-04 11:38:52 +0000

    deep field visitor

--- src/main/java/com/googlecode/classgenerator/DeepFieldVisitor.scala
+++ src/main/java/com/googlecode/classgenerator/DeepFieldVisitor.scala
@@ -9,18 +9,28 @@ import java.util.IdentityHashMap
  * 4 Jun 2012
  */
 class DeepFieldVisitor(
-		reflectionManager: ReflectionManager) {
+		reflectionManager: ReflectionManager,
+		dontVisit: Any => Boolean = DeepFieldVisitor.dontVisit) {
 
 	def visit[T, R](t: T)(visitor: (Any, Field, Any) => R): List[R] = visitInner(t, new IdentityHashMap, visitor)
 
 	private def visitInner[R](o: Any, m: IdentityHashMap[Any, Any], visitor: (Any, Field, Any) => R): List[R] =
 		if (!m.containsKey(o)) {
 			m.put(o, o)
-			val fields = reflectionManager.fields(o.getClass)
+			val fields = reflectionManager.fields(o.getClass).filterNot(_.getName == "$outer")
 			fields.map { field =>
 				val v = reflectionManager.get[Any, Any](field, o)
 				val r = visitor(o, field, v)
-				r :: (if (v == null) Nil else visitInner(v, m, visitor))
+				r :: (if (v == null) Nil else {
+					if (dontVisit(v))
+						Nil
+					else
+						visitInner(v, m, visitor)
+				})
 			}.flatten
 		} else Nil
+}
+
+object DeepFieldVisitor {
+	val dontVisit = (v: Any) => v.getClass.getName.startsWith("java.lang.")
 }
